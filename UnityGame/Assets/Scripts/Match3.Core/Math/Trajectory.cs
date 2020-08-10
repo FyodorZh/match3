63399e04c8317018ce13f79558063d443ff6aef3
Fyodor <fese@list.ru>
2020-08-10 22:53:20 +0200

    ITrajectory

---
+++ UnityGame/Assets/Scripts/Match3.Core/Math/Trajectory.cs
@@ -0,0 +1,10 @@
+﻿namespace Match3.Math
+{
+    public interface ITrajectory
+    {
+        FixedVector2 Offset { get; set; }
+        FixedVector2 Velocity { get; set; }
+
+        bool Update(Fixed timeSeconds);
+    }
+}
