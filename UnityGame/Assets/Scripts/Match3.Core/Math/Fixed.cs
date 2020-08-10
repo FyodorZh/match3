e3328fc8de864ee612fd1fc387da5364356efeef
Fyodor <fese@list.ru>
2020-08-10 22:25:50 +0200

    Fixed 2.0

---
+++ UnityGame/Assets/Scripts/Match3.Core/Math/Fixed.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Match3.Math
+{
+    public readonly struct Fixed : IEquatable<Fixed>
+    {
+        private const long Base = 1000;
+        private const double InvBase = 1.0 / Base;
+
+        private readonly long _value;
+
+        private Fixed(long value)
+        {
+            _value = value;
+        }
+
+        public override string ToString()
+        {
+            return ToFloat().ToString();
+        }
+
+        public float ToFloat()
+        {
+            return (float)(_value * InvBase);
+        }
+        
+        public static Fixed operator /(Fixed left, Fixed right)
+        {
+            return new Fixed(left._value * Base / right._value);
+        }
+
+        public bool Equals(Fixed other)
+        {
+            return _value == other._value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Fixed other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+
+        public static bool operator ==(Fixed left, Fixed right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Fixed left, Fixed right)
+        {
+            return !left.Equals(right);
+        }
+
+        public static implicit operator Fixed(int value)
+        {
+            return new Fixed(value * Base);
+        }
+
+        public static Fixed operator +(Fixed number)
+        {
+            return number;
+        }
+        
+        public static Fixed operator -(Fixed number)
+        {
+            return new Fixed(-number._value);
+        }
+
+        public static Fixed operator +(Fixed left, Fixed right)
+        {
+            return new Fixed(left._value + right._value);
+        }
+        
+        public static Fixed operator -(Fixed left, Fixed right)
+        {
+            return new Fixed(left._value - right._value);
+        }
+        
+        public static Fixed operator *(Fixed left, Fixed right)
+        {
+            return new Fixed(left._value * right._value / Base);
+        }
+    }
+}
---
+++ UnityGame/Assets/Scripts/Match3.Core/Math/FixedVector2.cs
@@ -0,0 +1,14 @@
+﻿namespace Match3.Math
+{
+    public struct FixedVector2
+    {
+        public Fixed X;
+        public Fixed Y;
+
+        public FixedVector2(Fixed x, Fixed y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
